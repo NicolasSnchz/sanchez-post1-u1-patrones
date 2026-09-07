00ca419b29c9d309843a11fd4e558d28d51c927f
NicolasSnchz <nicosanchezvil@gmail.com>
2026-09-07 03:51:35 -0500

    refactor(SRP): separar responsabilidades de OrderProcessor en clases cohesivas

---
+++ parte-1-refactorizacion-solid/src/main/java/com/patrones/u1/OrderReporter.java
@@ -0,0 +1,11 @@
+package com.patrones.u1;
+
+import java.util.List;
+
+public class OrderReporter {
+
+    public void print(List<String> orders) {
+        System.out.println("=== Reporte de Ordenes ===");
+        orders.forEach(o -> System.out.println("  " + o));
+    }
+}
